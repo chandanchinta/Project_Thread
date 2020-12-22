3f947dea5786794df3e18c8269857a692732ba97
kamujusrujan <kamujusrujan01@gmail.com>
2020-12-22 18:59:06 +0530

    added route API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Login from './components/Pages/Login';
-import Register from './components/Pages/Register';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Login from "./components/Pages/Login";
+import Register from "./components/Pages/Register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Switch>
-        <Route path='/' exact />
-        <Route path='/login' exact component={Login}/>
-        <Route path='/Register' exact component={Register}/>
-      </Switch>
-    </Router>
-      
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/login" exact component={Login} />
+          <Route path="/Register" exact component={Register} />
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,57 +1,70 @@
-import React,{useState} from 'react';
-import {Link} from 'react-router-dom';
-import { Button } from './Button';
-import './Navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 
-function Navbar(){
-    const [click,setClick]=useState(false);
+function Navbar() {
+    const [click, setClick] = useState(false);
     const [button, setButton] = useState(true);
     const handleclick = () => setClick(!click);
-    const closeMenu = () => setClick(false); 
+    const closeMenu = () => setClick(false);
 
-    const showButton = () =>{
-        if(window.innerwidth <= 960){
-            setButton(false)
-        }else{
-            setButton(true)
+    const showButton = () => {
+        if (window.innerwidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
         }
     };
 
-    window.addEventListener('resize',showButton);
+    window.addEventListener("resize", showButton);
 
-    
-    return(
+    return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to='/' className='navbar-logo' onClick={closeMenu}>
-                        Project Thread 
+                    <Link to="/" className="navbar-logo" onClick={closeMenu}>
+                        Project Thread
                     </Link>
-                    <div className='menu-icon' onClick={handleclick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                    <div className="menu-icon" onClick={handleclick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
                     </div>
-                    <ul className={click ? 'nav-menu active': 'nav-menu'}>
-                        <li className ='nav-item'>
-                            <Link to = '/Login' className='nav-links' onClick={closeMenu}>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link
+                                to="/Login"
+                                className="nav-links"
+                                onClick={closeMenu}
+                            >
                                 Login
                             </Link>
                         </li>
-                        <li className ='nav-item'>
-                        <Link to = '/Register' className='nav-links' onClick={closeMenu}>
+                        <li className="nav-item">
+                            <Link
+                                to="/Register"
+                                className="nav-links"
+                                onClick={closeMenu}
+                            >
                                 Register
                             </Link>
                         </li>
-                        <li className ='nav-item'>
-                        <Link to = '/Shop' className='nav-links' onClick={closeMenu}>
+                        <li className="nav-item">
+                            <Link
+                                to="/Shop"
+                                className="nav-links"
+                                onClick={closeMenu}
+                            >
                                 Shop
                             </Link>
                         </li>
                     </ul>
-                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                    {button && (
+                        <Button buttonStyle="btn--outline">SIGN UP</Button>
+                    )}
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
 export default Navbar;
