27d6dfdf24d6670d2d83e10e9228e1210dfe4bfc
kamujusrujan <kamujusrujan01@gmail.com>
2020-12-28 03:09:26 +0530

    added Modal

--- src/components/Pages/Shop/Item.js
+++ src/components/Pages/Shop/Item.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import IconButton from "@material-ui/core/IconButton";
 import FavoriteBorderRoundedIcon from "@material-ui/icons/FavoriteBorderRounded";
-import { Carousel, Modal, Card } from "bootstrap";
+import { Carousel, Modal, Card, Button } from "react-bootstrap";
 
 function ItemModal(props) {
   return (
@@ -12,6 +12,7 @@ function ItemModal(props) {
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      style={{ height: "90%" }}
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
@@ -19,30 +20,7 @@ function ItemModal(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>{props.BasePrice}</h4>
-        <p>{props.Description}</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-
-function Item(props) {
-  const [Modal, setModal] = useState(false);
-  const OpenModal = () => {
-    setModal(true);
-  };
-  const CloseModal = () => {
-    setModal(false);
-  };
-
-  return (
-    <a style={{ cursor: "pointer" }} onClick={OpenModal}>
-      <Card className="bg-white text-black ">
-        {/* <Card.Img src={product.ImageURL} alt="Card image" /> */}
-        <Carousel>
+        <Carousel style={{ width: "auto", height: "auto" }}>
           <Carousel.Item>
             <img
               className="d-block w-100"
@@ -51,26 +29,68 @@ function Item(props) {
             />
           </Carousel.Item>
         </Carousel>
-        <Card.Title>
-          <center>
-            <h3>{props.Name}</h3>
-            <div>
-              <IconButton>
-                <AddShoppingCartIcon />
-              </IconButton>
-              <IconButton>
-                <FavoriteBorderRoundedIcon />
-              </IconButton>
-            </div>
-          </center>
-        </Card.Title>
-        <Card.Body style={{ padding: "2rem" }}>
-          <Card.Text>{props.Description}</Card.Text>
-          <Card.Text>{props.BasePrice}</Card.Text>
-        </Card.Body>
-      </Card>
-      <ItemModal show={Modal} closeModal={closeModal} {...props} />
-    </a>
+        <h4>
+          <strong>{props.BasePrice}</strong>
+        </h4>
+        <p>{props.Description}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <div>
+          <IconButton>
+            <AddShoppingCartIcon />
+          </IconButton>
+          <IconButton>
+            <FavoriteBorderRoundedIcon />
+          </IconButton>
+        </div>
+        <Button onClick={props.closeModal}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function Item(props) {
+  const [modal, setModal] = useState(false);
+  const OpenModal = () => {
+    setModal(true);
+  };
+  const CloseModal = () => {
+    setModal(false);
+  };
+  return (
+    <div>
+      <a style={{ cursor: "pointer" }} onClick={OpenModal}>
+        <Card className="bg-white text-black ">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={props.ImageURL}
+                alt="First slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+          <Card.Title>
+            <center>
+              <h3>{props.Name}</h3>
+              <div>
+                <IconButton>
+                  <AddShoppingCartIcon />
+                </IconButton>
+                <IconButton>
+                  <FavoriteBorderRoundedIcon />
+                </IconButton>
+              </div>
+            </center>
+          </Card.Title>
+          <Card.Body style={{ padding: "2rem" }}>
+            <Card.Text>{props.Description}</Card.Text>
+            <Card.Text>{props.BasePrice}</Card.Text>
+          </Card.Body>
+        </Card>
+      </a>
+      <ItemModal Show={modal} closeModal={CloseModal} {...props} />
+    </div>
   );
 }
 
