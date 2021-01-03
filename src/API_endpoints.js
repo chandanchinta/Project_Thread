a6e1d0f2993fd485d88d94eac781842529e2d3bf
kamujusrujan <kamujusrujan01@gmail.com>
2021-01-03 11:48:21 +0530

    pushing live

--- src/API_endpoints.js
+++ src/API_endpoints.js
@@ -1,6 +1,6 @@
-// const WEB_HOST = "https://nameless-sands-40669.herokuapp.com";
+const WEB_HOST = "https://nameless-sands-40669.herokuapp.com";
 
-const WEB_HOST = "http://localhost:8080"; // <--------- used for local testing
+// const WEB_HOST = "http://localhost:8080"; // <--------- used for local testing
 
 export const API_LOGIN = WEB_HOST + "/userAuth/login";
 
