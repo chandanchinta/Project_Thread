3f0c3c8f817900a6098ff59fb8b8616c49d34dc1
kamujusrujan <kamujusrujan01@gmail.com>
2021-01-03 13:00:15 +0530

    added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import store from "./components/utils/Redux/store";
 import { Provider } from "react-redux";
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
